5e038fc44893ea54f321b61c1fcabe55d22c4d20
toivan <33359118+toivan@users.noreply.github.com>
2021-11-27 10:03:44 +0300

    Create SpaceDoggos.sol

---
+++ SpaceDoggos.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.10;
+
+contract SpaceDoggos {
+
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
+
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
+
+    uint planetCodeModulus = 10 ** planetCodeDigits;
+    uint systemCodeModulus = 10 ** systemCodeDigits;
+
+    // Write your solution below
+    struct Doggo {
+      string name;
+      uint8 breed;
+      uint8 color;
+      uint8 face;
+uint8 with name costume
+uint with name coordX
+uint with name coordY
+    }
+
+
+}
